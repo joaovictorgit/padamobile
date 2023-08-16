284e007140594eceef5836bccf2afb8dd21d69e2
joaovictorgit <victoralmeida.0216@gmail.com>
2023-08-16 14:22:18 -0300

    First test ci/cd

--- AppOld.tsx
+++ AppOld.tsx
@@ -10,3 +10,41 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+/**
+ *APP_IDENTIFIER = "com.padamobile.teste"
+PROJECT_NAME = "padamobile"
+GIT_ACCESS = ENV["GIT_ACCESS"]
+
+update_fastlane if is_ci
+
+before_all do
+    ENV["CI_KEYCHAIN_NAME"] ||= "keychain_nome_aleatorio"
+    ENV["CI_KEYCHAIN_PASSWORD"] ||= "keychain_senha_aleatoria"
+end
+
+platform :android do
+    desc "Deliver QA version to QA team"
+    lane :deploy_qa do
+        gradle(task: 'clean', project_dir: 'android/')
+
+        gradle(
+            project_dir: "android/"
+            task: "assemble",
+            flavor: "Staging",
+            build_type: "Release"
+        )
+    end
+
+    desc "Deliver production version to Google Play Console"
+    lane :deploy_production do
+        gradle(
+            project_dir: "android/",
+            task: "assemble",
+            flavor: "Production",
+            build_type: "Release"
+        )
+        upload_to_play_store(track: 'production')
+    end
+end
+ */
